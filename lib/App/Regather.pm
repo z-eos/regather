517ff64cc75013e69c454ba4fc1a47e5b8831ef2
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2021-04-21 11:18:26 +0300

    version incremented

--- lib/App/Regather.pm
+++ lib/App/Regather.pm
@@ -40,7 +40,7 @@ use App::Regather::Plugin;
 use constant SYNST => [ qw( LDAP_SYNC_PRESENT LDAP_SYNC_ADD LDAP_SYNC_MODIFY LDAP_SYNC_DELETE ) ];
 
 # my @DAEMONARGS = ($0, @ARGV);
-our $VERSION   = '0.80.04';
+our $VERSION   = '0.81.00';
 
 sub new {
   my $class = shift;
